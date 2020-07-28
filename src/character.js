df4dd7541ec5c77131203b4b244b844e4fd83c31
Svetlana Ryabova <ganyim@inbox.ru>
2020-07-28 23:25:01 +0500

    method add

--- src/character.js
+++ src/character.js
@@ -2,28 +2,16 @@ export const arrayCharacter = [];
 export class Character {
   constructor(name) {
     this.name = name;
-    this.group();
+  //  this.group();
   }
 
-  group() {
-    arrayCharacter.push(this.name);
-    return arrayCharacter;
-  }
+  // group() {
+  //   arrayCharacter.push(this.name);
+  //   return arrayCharacter;
+  // }
 
-  set addPerson(value) {
-    this.name = value;
-    this.group();
-  }
+  // set addPerson(value) {
+  //   this.name = value;
+  //   this.group();
+  // }
 }
-// const ch1 = new Character('George', 'Clooney');
-// console.log(arrayCharacter);
-// ch1.addPerson = ['Lady', 'Gaga'];
-// console.log(arrayCharacter);
-// ch1.addPerson = ['Evgeniy', 'Onegin'];
-// console.log(arrayCharacter);
-// ch1.addPerson = ['Vladimir', 'Putin'];
-// console.log(arrayCharacter);
-// ch1.addPerson = ['Innokentiy', 'Frolov'];
-// console.log(arrayCharacter);
-// ch1.addPerson = ['Roman', 'Kushnarev'];
-// console.log(arrayCharacter);
--- src/index.js
+++ src/index.js
@@ -1,34 +1,44 @@
-import { arrayCharacter, Character } from './character.js';
+import { Character } from './character.js';
 
 export default class Team {
   constructor() {
-    this.members = new Set();  
+    this.members = new Set();
   }
 
-  add() {
-    arrayCharacter.forEach((arr) => {
-      this.members.add(arr);
-    });
-    console.log(this.members);
-    arrayCharacter.forEach((arr) => {
-      if (this.members.has(arr)) {
-        throw new Error(`${arr} уже добавлен в команду`);
-      }
-    });
-    return this.members;
+  add(value) {
+    if (this.members.has(value.name)) {
+      throw new Error(`Игрок ${value.name} уже добавлен в команду`);
+    }
+    this.members.add(value.name);
   }
 }
-
-const ch1 = new Character('George Clooney');
-ch1.addPerson = 'Lady Gaga';
-ch1.addPerson = 'Evgeniy Onegin';
-ch1.addPerson = 'Vladimir Putin';
-ch1.addPerson = 'Innokentiy Frolov';
-ch1.addPerson = 'Roman Kushnarev';
-ch1.addPerson = 'Roman Kushnarev';
-
-console.log(arrayCharacter);
-
 const team = new Team();
+const ch1 = new Character('George Clooney');
+team.add(ch1);
 console.log(team);
-console.log(team.add());
+const ch2 = new Character('Lady Gaga');
+team.add(ch2);
+console.log(team);
+const ch3 = new Character('Lady Gaga');
+team.add(ch3);
+console.log(team);
+
+// const ch3 = new Character('George Clooney');
+// const ch4 = new Character('George Clooney');
+// const ch5 = new Character('George Clooney');
+// const ch6 = new Character('George Clooney');
+// const ch7 = new Character('George Clooney');
+// const ch8 = new Character('George Clooney');
+// ch1.addPerson = 'Lady Gaga';
+// ch1.addPerson = 'Evgeniy Onegin';
+// ch1.addPerson = 'Vladimir Putin';
+// ch1.addPerson = 'Innokentiy Frolov';
+// ch1.addPerson = 'Roman Kushnarev';
+// ch1.addPerson = 'Roman Kushnarev';
+
+// console.log(arrayCharacter);
+// const team = new Team();
+// team.clear();
+// console.log(team);
+// console.log(team.add());
+// console.log(team);
